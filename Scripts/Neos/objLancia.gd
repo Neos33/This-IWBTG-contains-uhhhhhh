3cc2f3d7f19b9741061d2c3368d6acfe1141bf65
Neos33 <46664910+Neos33@users.noreply.github.com>
2024-06-05 16:15:30 -0300

    did it apply the changes?

--- Scripts/Neos/objLancia.gd
+++ Scripts/Neos/objLancia.gd
@@ -27,6 +27,8 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	# Not able to move if we already crashed
 	if not crashed:
 		move_and_slide()
 		
@@ -40,8 +42,9 @@ func _on_block_detection_body_entered(body):
 		var _tween = create_tween()
 		_tween.tween_property(path_follow_2d, "progress_ratio", 1, 3.0)
 		_tween.set_parallel().tween_property(car, "rotation", 4, 3.0)
+		
 		car.visible = true
 		sprite_car.visible = false
-		
-	block_detection.disabled = true
-	hitbox.disabled = true
+		block_detection.disabled = true
+		hitbox.disabled = true
+
